22d9b84523a9c626938e3255b305877f165dcd16
Umakant <dumakant19@gmail.com>
2018-02-24 18:04:15 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -36,5 +36,5 @@ colnames(X_total) <- variable_names[selected_var[,1],2]
 # of each variable for each activity and each subject.
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
-total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_all(funs(mean))
 write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
